4b1e3b5bae74a4608c0faef3d85148e89fefa1fb
sahadeo <nimaitradeenabler@gmail.com>
2020-09-11 19:09:34 +0530

    Country and goods issue resolved

--- nimaiUCM/src/main/java/com/nimai/ucm/repository/BlackListedGoodsRepository.java
+++ nimaiUCM/src/main/java/com/nimai/ucm/repository/BlackListedGoodsRepository.java
@@ -3,6 +3,9 @@ package com.nimai.ucm.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.nimai.ucm.entity.BlackListedGoods;
 import com.nimai.ucm.entity.NimaiCustomer;
@@ -11,6 +14,8 @@ public interface BlackListedGoodsRepository extends JpaRepository<BlackListedGoo
 
 	List<BlackListedGoods> findByUserId(NimaiCustomer nc);
 	
-	
+	@Modifying
+	@Query("delete from BlackListedGoods b where b.userId.userid =:userId")
+	void deleteBlackListedGoodsUserId(@Param("userId") String userId);
 
 }
--- nimaiUCM/src/main/java/com/nimai/ucm/repository/InterestedCountryRepository.java
+++ nimaiUCM/src/main/java/com/nimai/ucm/repository/InterestedCountryRepository.java
@@ -3,6 +3,9 @@ package com.nimai.ucm.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.nimai.ucm.entity.InterestedCountry;
 import com.nimai.ucm.entity.NimaiCustomer;
@@ -10,5 +13,11 @@ import com.nimai.ucm.entity.NimaiCustomer;
 public interface InterestedCountryRepository extends JpaRepository<InterestedCountry, Long> {
 
 	List<InterestedCountry> findByUserId(NimaiCustomer nc);
+	
+	@Modifying
+	@Query("delete from InterestedCountry b where b.userId.userid =:userId")
+	void deleteInterestedCountryUserId(@Param("userId") String userId);
+
+
 
 }
